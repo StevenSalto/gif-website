23545dd928dfe3b8a0ac6899f8d50a3aa421ce71
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-06-09 00:39:28 -0400

    Implemented getResults and displayResults function. Site now displays 9 gifs upon a submit event

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const SEARCH_BAR= document.querySelector("#searchBar")
 const BTN = document.querySelector("#submitButton")
+const FORM = document.querySelector("#form")
 const GIF_SECTION = document.querySelector("#gifSection")
 const API_KEY = 'z9qEJKCAcrrMCA7q77qHJXLDkv111Kra';
 
@@ -9,9 +10,27 @@ let rating = 'g';
 
 let url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchPhrase}&limit=${limit}&rating=${rating}`;
 
+FORM.addEventListener("submit", test)
+
+function test(evt) {
+    evt.preventDefault();
+    console.log("test");
+    getResults();
+}
 async function getResults() {
+    console.log("getResults")
     let resp =  await fetch(url);
     let response =  await resp.json();
     console.log(response);
-    GIF_SECTION.innerHTML = `<img src="${response.data[1].images.original.url}">`
+    //GIF_SECTION.innerHTML = `<img src="${response.data[1].images.original.url}">`
+    displayResults(response.data);
 }
+
+function displayResults(gifs) {
+    console.log("displayResults")
+    gifs.forEach(element => {
+        console.log(element)
+        GIF_SECTION.innerHTML += `<img src='${element.images.original.url}'>`
+    });
+}
+console.log("eof")
