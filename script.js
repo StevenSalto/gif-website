fb4de8228aff3efff4d79b2ed405a3e51938c95e
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-06-09 02:06:39 -0400

    Implemented handleShowMore, a function that shows more gifs at the click of a button

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const BTN = document.querySelector("#submitButton")
 const FORM = document.querySelector("#form")
 let GIF_SECTION = document.querySelector("#gifSection")
 const API_KEY = 'z9qEJKCAcrrMCA7q77qHJXLDkv111Kra';
-
+const ADD_MORE_GIFS_BTN = document.querySelector(".hidden");
 let searchPhrase = 'naruto';
 let limit = 9;
 let rating = 'g';
-
-let url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchPhrase}&limit=${limit}&rating=${rating}`;
+let pages = 0;
+let offset = pages * limit;
+let url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchPhrase}&limit=${limit}&rating=${rating}&offset=${offset}`;
 
 FORM.addEventListener("submit", handleFormSubmit)
+ADD_MORE_GIFS_BTN.addEventListener("click", handleShowMore)
 
 function test(evt) {
     evt.preventDefault();
@@ -37,8 +39,15 @@ function handleFormSubmit(evt) {
     GIF_SECTION.innerHTML = "";
     searchPhrase = SEARCH_BAR.value;
     SEARCH_BAR.value = "";
-    url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchPhrase}&limit=${limit}&rating=${rating}`;
+    url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchPhrase}&limit=${limit}&rating=${rating}&offset=${offset}`;
+    ADD_MORE_GIFS_BTN.classList.remove("hidden");
     getResults();
 }
 
-console.log("eof")
+function handleShowMore(evt) {
+    evt.preventDefault();
+    pages += 1;
+    let offset = pages * limit;
+    url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchPhrase}&limit=${limit}&rating=${rating}&offset=${offset}`;
+    getResults();
+}
